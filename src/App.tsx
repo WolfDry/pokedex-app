6edfe91033e24a48eb26aa0ba486d8e1901d6775
Benjamin <benjamindeltour22@gmail.com>
2024-04-08 09:28:32 +0200

    change the top button to be a component

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,12 @@
 import React from 'react';
 import Pokedex from './components/Pokedex/Pokedex';
+import TopButton from './components/TopButton';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Pokedex />
-      <button className='top' onClick={() => {
-        window.scrollTo({
-          top: 0,
-          behavior: 'smooth'
-        })
-      }}>
-        <div className="text">
-          <span>Back</span>
-          <span>to</span>
-          <span>top</span>
-        </div>
-        <div className="clone">
-          <span>Back</span>
-          <span>to</span>
-          <span>top</span>
-        </div>
-        <svg
-          stroke-width="2"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          fill="none"
-          className="h-6 w-6"
-          xmlns="http://www.w3.org/2000/svg"
-          width="20px"
-        >
-          <path
-            d="M14 5l7 7m0 0l-7 7m7-7H3"
-            stroke-linejoin="round"
-            stroke-linecap="round"
-          ></path>
-        </svg>
-      </button>
-
+      <TopButton />
     </div>
   );
 };
---
+++ src/components/TopButton.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const TopButton: React.FC = () => {
+  return (
+    <button className='top' onClick={() => {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      })
+    }}>
+      <div className="text">
+        <span>Back</span>
+        <span>to</span>
+        <span>top</span>
+      </div>
+      <div className="clone">
+        <span>Back</span>
+        <span>to</span>
+        <span>top</span>
+      </div>
+      <svg
+          stroke-width="2"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          fill="none"
+          className="h-6 w-6"
+          xmlns="http://www.w3.org/2000/svg"
+          width="20px"
+        >
+          <path
+            d="M14 5l7 7m0 0l-7 7m7-7H3"
+            stroke-linejoin="round"
+            stroke-linecap="round"
+          ></path>
+        </svg>
+    </button>
+  );
+};
+
+export default TopButton;
